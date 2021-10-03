7519a77946c93457d19ac9627436d59693c1fb86
connormullett <connormullett@gmail.com>
2021-10-03 05:55:18 -0400

    add mods

---
+++ src/arena.rs
@@ -0,0 +1,88 @@
+use crate::Node;
+
+#[derive(Default)]
+pub struct Arena<T, K>
+where
+    T: Default,
+    K: Default,
+{
+    nodes: Vec<Node<T, K>>,
+}
+
+impl<T, K> Arena<T, K>
+where
+    K: PartialEq + Default,
+    T: Default,
+{
+    pub fn new_default() -> Self {
+        Self::default()
+    }
+
+    pub fn new() -> Self {
+        Self { nodes: vec![] }
+    }
+
+    pub fn insert(&mut self, data: T, key: K) -> usize {
+        let next_index = self.nodes.len();
+
+        self.nodes.push(Node::new(data, key));
+
+        next_index
+    }
+
+    pub fn find_inner(&self, key: K) -> Option<&T> {
+        for node in self.nodes.iter() {
+            if node.key == key {
+                return Some(&node.data);
+            }
+        }
+
+        None
+    }
+
+    pub fn get_inner(&self, index: usize) -> Option<&T> {
+        let node = self.nodes.get(index);
+
+        if let Some(node) = node {
+            Some(&node.data)
+        } else {
+            None
+        }
+    }
+
+    pub fn get_inner_mut(&mut self, index: usize) -> Option<&mut T> {
+        let node = self.nodes.get_mut(index);
+
+        if let Some(node) = node {
+            Some(&mut node.data)
+        } else {
+            None
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&Node<T, K>> {
+        self.nodes.get(index)
+    }
+
+    pub fn get_mut(&mut self, index: usize) -> Option<&mut Node<T, K>> {
+        self.nodes.get_mut(index)
+    }
+
+    pub fn find(&self, key: K) -> Option<&Node<T, K>> {
+        for node in self.nodes.iter() {
+            if node.key == key {
+                return Some(node);
+            }
+        }
+
+        None
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.nodes.len() == 0
+    }
+
+    pub fn count(&self) -> usize {
+        self.nodes.len()
+    }
+}
---
+++ src/node.rs
@@ -0,0 +1,44 @@
+pub struct Node<T, K> {
+    parent: Option<usize>,
+    previous_sibling: Option<usize>,
+    next_sibling: Option<usize>,
+    first_child: Option<usize>,
+    last_child: Option<usize>,
+
+    pub key: K,
+    pub data: T,
+}
+
+impl<T, K> Node<T, K> {
+    pub fn new(data: T, key: K) -> Self {
+        Node {
+            parent: None,
+            previous_sibling: None,
+            next_sibling: None,
+            first_child: None,
+            last_child: None,
+            key,
+            data,
+        }
+    }
+
+    pub fn parent(&self) -> Option<usize> {
+        self.parent
+    }
+
+    pub fn previous_sibling(&self) -> Option<usize> {
+        self.previous_sibling
+    }
+
+    pub fn next_sibling(&self) -> Option<usize> {
+        self.next_sibling
+    }
+
+    pub fn first_child(&self) -> Option<usize> {
+        self.first_child
+    }
+
+    pub fn last_child(&self) -> Option<usize> {
+        self.last_child
+    }
+}
