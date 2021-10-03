b3ab01ca5510d9fbdf9c4f98e3578ad6cbf67c03
connormullett <connormullett@gmail.com>
2021-10-03 05:55:09 -0400

    kick it out to modules and basic methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,5 @@
-pub struct Arena<T, K> {
-    nodes: Vec<Node<T, K>>,
-}
+mod arena;
+mod node;
 
-struct Node<T, K> {
-    parent: Option<NodeId>,
-    previous_sibling: Option<NodeId>,
-    next_sibling: Option<NodeId>,
-    first_child: Option<NodeId>,
-    last_child: Option<NodeId>,
-
-    pub key: K,
-    pub data: T,
-}
-
-pub struct NodeId {
-    index: usize,
-}
-
-impl<T, K> Arena<T, K>
-where
-    K: PartialEq,
-{
-    pub fn insert(&mut self, data: T, key: K) -> NodeId {
-        // Get the next free index
-        let next_index = self.nodes.len();
-
-        // Push the node into the arena
-        self.nodes.push(Node {
-            parent: None,
-            first_child: None,
-            last_child: None,
-            previous_sibling: None,
-            next_sibling: None,
-            key,
-            data,
-        });
-
-        // Return the node identifier
-        NodeId { index: next_index }
-    }
-
-    pub fn get(&self, key: K) -> Option<&T> {
-        for node in self.nodes.iter() {
-            if node.key == key {
-                return Some(&node.data);
-            }
-        }
-
-        None
-    }
-}
+pub use arena::*;
+pub use node::*;
